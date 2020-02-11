9b541c4a38a574dd0f8086f5ae409077c82b60d8
Jayden Rasmussen <jayden.rasmussen@chghealthcare.com>
2020-02-11 09:58:22 -0700

    fix: removed double defining environment variables

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@
 # Write the global environment (used for cleaning up when the runner is deleted)
 echo "GITHUB_OWNER=${GITHUB_OWNER}" >> /etc/environment
 echo "GITHUB_REPO=${GITHUB_REPO}" >> /etc/environment
-echo "GITHUB_TOKEN=${GITHUB_TOKEN}" > /etc/environment
+echo "GITHUB_TOKEN=${GITHUB_TOKEN}" >> /etc/environment
